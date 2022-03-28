1ccc4208e8f74eb159715da5a5eb2603b02d0137
KaiserSov <sara310105@gmail.com>
2022-03-28 12:10:37 -0500

    Agregué las plantas como objetos

---
+++ src/main/java/com/cultivo/DionaeaMuscipula.java
@@ -0,0 +1,4 @@
+package com.cultivo;
+
+public class DionaeaMuscipula {
+}
--- src/main/java/com/cultivo/Main.java
+++ src/main/java/com/cultivo/Main.java
@@ -1,4 +1,21 @@
 package com.cultivo;
 
+import com.cultivo.Drosera;
+
 public class Main {
+    public static void main(String[] args) {
+
+        Drosera dro;
+        dro = new Drosera();
+
+        String col = dro.obtenerColor();
+        String altu = dro.obtenerAltura();
+        String temp = dro.obtenerTemperatura();
+        String ori = dro.obtenerOrigen();
+        String tip = dro.obtenerTipo();
+        dro.regarDrosera();
+
+        System.out.println("El color de la planta es: "+col);
+
+    }
 }
